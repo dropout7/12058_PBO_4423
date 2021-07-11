c28da1e8ffa4608878c0bd71d6c40361757c0719
sat <sat@mbs-PC>
2021-07-11 21:56:04 +0700

    uas pbo

--- src/database/KoneksiDb.java
+++ src/database/KoneksiDb.java
@@ -1,11 +1,11 @@
 package database;
 import java.sql.*;
 import javax.swing.JOptionPane;
-public class RentDatabase {
+public class KoneksiDb {
 	public static Connection Koneksi() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/sewabuku", "root", "dhevan007");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:/db_coba", "root", "");
             return connection;
         } catch(Exception e) {
             JOptionPane.showMessageDialog(null, e);
